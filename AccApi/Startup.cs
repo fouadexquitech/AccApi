1cfd0e12030f33414a777e53402f49781683ada7
Fouad Kassouf <fouad.kassouf@exquitech.com>
2022-04-13 10:44:20 +0300

    2022-04-13 1

--- AccApi/Startup.cs
+++ AccApi/Startup.cs
@@ -3,6 +3,7 @@ using AccApi.Repository.Interfaces;
 using AccApi.Repository.Managers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -35,10 +36,7 @@ namespace AccApi
                     });
             });
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AccApi", Version = "v1" });
-            });
+           
 
             services.AddDbContext<AccDbContext>(opts => opts.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddDbContext<MasterDbContext>(opts => opts.UseSqlServer(Configuration.GetConnectionString("MasterConnection")));
@@ -52,8 +50,16 @@ namespace AccApi
             services.AddTransient<IRevisionDetailsRepository, RevisionDetailsRepository>();
             services.AddTransient<IlogonRepository, LogonRepository>();
             services.AddTransient<IConditionsRepository, ConditionsRepository>();
+            services.AddTransient<IComparisonGroupRepository, ComparisonGroupRepository>();
 
             services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AccApi", Version = "v1" });
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
