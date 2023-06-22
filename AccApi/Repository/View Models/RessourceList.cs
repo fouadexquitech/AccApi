5606d83a5094c381c0e9a39d8b6624bef8df6025
Abed Hijazi <ahijazi@accsal.com>
2023-06-22 10:09:23 +0300

    abed

---
+++ AccApi/Repository/View Models/RessourceList.cs
@@ -0,0 +1,10 @@
+﻿namespace AccApi.Repository.View_Models
+{
+    public class RessourceList
+    {
+        public string resSeq { get; set; }
+        public string resDesc { get; set; }
+    }
+
+
+}
