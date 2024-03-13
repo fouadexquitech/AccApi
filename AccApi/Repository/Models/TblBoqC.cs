755c0e9dae4ab44fe0f347caed09284ab7a83b85
Abed Hijazi <ahijazi@accsal.com>
2024-03-13 14:59:30 +0200

    ok

---
+++ AccApi/Repository/Models/TblBoqC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace AccApi.Repository.Models
+{
+    [Table("tblBoqC")]
+    public partial class TblBoqC
+    {
+        [Key]
+        [Column("bcBoqItem")]
+        [StringLength(25)]
+        public string BcBoqItem { get; set; }
+        [Key]
+        [Column("bcCSeq")]
+        public int BcCseq { get; set; }
+    }
+}
---
+++ AccApi/Repository/Models/TblBoqLevel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace AccApi.Repository.Models
+{
+    [Table("tblBoqLevel")]
+    public partial class TblBoqLevel
+    {
+        [Key]
+        [Column("blBoqItem")]
+        [StringLength(25)]
+        public string BlBoqItem { get; set; }
+        [Key]
+        [Column("blLevelSeq")]
+        public int BlLevelSeq { get; set; }
+    }
+}
