6d8e2ea110540f90881c6dccbd7cb6dfa1778439
Fouad Kassouf <fouad.kassouf@exquitech.com>
2024-12-22 19:08:32 +0200

    x

---
+++ AccApi/Repository/View Models/Common/ResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AccApi.Repository.View_Models.Common
+{
+    public class ResponseModel<T>
+    {
+        public bool Success { get; set; } = true;
+
+        public string Message { get; set; }
+
+        public T Result { get; set; }
+    }
+}
